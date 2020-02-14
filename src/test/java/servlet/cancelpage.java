0c751b217caab0b5eb5f515be4ba834d579f2f9d
NAGA SAI PEDDETI <1185428@india.tcs.com>
2020-02-13 23:23:03 -0800

    made ip changes

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://3.135.65.115:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://database:5432/postgres";
 					String username="postgres";
 					String password="";
 					
